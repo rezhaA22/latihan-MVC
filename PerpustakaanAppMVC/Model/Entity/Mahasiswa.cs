2675370e3294c27fa9e242faf19592fabe8b76c3
rez <127712669+rezhaA22@users.noreply.github.com>
2023-12-04 21:22:52 +0700

    Update Mahasiswa.cs
    test

--- PerpustakaanAppMVC/Model/Entity/Mahasiswa.cs
+++ PerpustakaanAppMVC/Model/Entity/Mahasiswa.cs
@@ -8,7 +8,7 @@ namespace PerpustakaanAppMVC.Model.Entity
 {
     public class Mahasiswa
     {
-        public string Npm { get; set; }
+        public string Npm { get; set; }  
         public string Nama { get; set; }
         public string Angkatan { get; set; }
     }
